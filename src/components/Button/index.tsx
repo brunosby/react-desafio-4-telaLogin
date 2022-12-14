07e3d77d29e95aa68a620b2447c83f3af55703be
brunosby <brunoshibuya2002@gmail.com>
2022-12-14 18:14:45 -0300

    desabilitando botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,12 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, isValid}: IButtonProps) => {
+  if(isValid){
+    return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+  } else {
+    return <></>;
+  }
 };
 
 export default Button;
